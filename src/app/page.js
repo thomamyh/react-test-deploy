e6ae046e522570f7a9b8ed13615b952502035833
Thomas Myhre <thomassmy@gmail.com>
2023-11-15 22:04:26 +0100

    Changed to keybased login

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing, Thomas!
+          Get started by editing here, Thomas!
         </p>
         <div>
           <a
