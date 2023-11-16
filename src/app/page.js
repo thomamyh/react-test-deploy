6b7d85c734797452e50e92ec106c1c90e603d99e
Thomas Myhre <thomassmy@gmail.com>
2023-11-16 09:50:07 +0100

    Changed text

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing here and now, Thomas!
+          Start editing here and now, Thomas!
         </p>
         <div>
           <a
